a0120edf62bbf59556f58e2a6c0257df51f29b15
felipemesquita <fmesquitacunha@gmail.com>
2009-03-15 21:43:58 -0300

    Improving base.rb file.

--- base.rb
+++ base.rb
@@ -1,5 +1,7 @@
+has_authentication = yes?("Should this project have an authentication support?")
+
 # All about gems.
-gem "authlogic" if yes?("Should this project have an authentication support?")
+gem "authlogic" if has_authentication
 gem "cucumber"
 
 # All about plugins.
@@ -15,19 +17,20 @@ rake "features"
 # Needed generates
 generate :rspec
 
+# 
+
 # Generating home.
 run "rm public/index.html"
 generate :controller, "home"
 route "map.root :controller => :home"
 
 # Model about auth.
-generate :model, "user", "login:string", "crypted_password:string", "password_salt:string", "persistence_token:string", "login_count:integer", "last_request_at:datetime", "last_login_at:datetime", "current_login_at:datetime", "last_login_ip:string", "current_login_ip:string"
+generate :model, "user", "login:string", "crypted_password:string", "password_salt:string", "persistence_token:string", "login_count:integer", "last_request_at:datetime", "last_login_at:datetime", "current_login_at:datetime", "last_login_ip:string", "current_login_ip:string" if has_authentication
 
 # Migrations.
-rake "db:migrate"
+rake "db:migrate" if yes?("Should perform migrations?")
 
 # All about git.
 git :init
 git :add => "."
 git :commit => "-a -m 'Initial commit.'"
-
